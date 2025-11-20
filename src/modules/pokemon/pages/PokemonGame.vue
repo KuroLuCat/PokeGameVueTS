5336468908f1e2aba0185ff2c54b0d68ed698328
KuroLuCat <lucat.rv7@gmail.com>
2025-11-20 14:08:37 -0500

    20112025

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,62 @@
+<template>
+  <section
+    v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h1 class="animate-pulse">Cargando Pokemons</h1>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quien es este pokemon?</h1>
+    <!-- <h3 class="capitalize">{{ gameStatus }}</h3> -->
+
+    <div class="h-20">
+      <button
+        v-if="gameStatus !== GameStatus.Playing"
+        @click="getNextRound()"
+        class="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 transition-alls"
+      >
+        ¿Jugar de nuevo?
+      </button>
+    </div>
+
+    <!-- <button @click="getNextRound()" :hidden="gameStatus === GameStatus.Playing">Nuevo juego</button> -->
+
+    <!-- POkemon picture -->
+    <PokemonPicture
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
+    <!-- POkemon options -->
+    <PokemonOptions
+      :options="options"
+      :block-selection="gameStatus !== GameStatus.Playing"
+      :correct-answer="randomPokemon.id"
+      @selected-option="checkAnswer"
+    />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
+
+const {
+  randomPokemon,
+  isLoading,
+  gameStatus,
+  pokemonOptions: options,
+  getNextRound,
+  checkAnswer,
+} = usePokemonGame();
+</script>
+
+<style scoped>
+/* @reference "tailwindcss";
+button {
+  @apply bg-white shadow-md rounded-lg p-1 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100;
+} */
+</style>
